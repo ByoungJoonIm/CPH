39895cbf9cb00eba60e8051d0ac5d591066b2023
BJ-Lim <fortress0427@gmail.com>
2020-01-07 14:44:16 +0900

    removing some files

--- auto_script/auto_install.sh
+++ auto_script/auto_install.sh
@@ -12,5 +12,4 @@ git clone https://github.com/BJ-Lim/Capstone_Design.git
 apt-get install software-properties-common
 apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
 add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://ftp.kaist.ac.kr/mariadb/repo/10.2/ubuntu xenial main'
-apt update
 apt install mariadb-server -y
