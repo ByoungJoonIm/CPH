52cbec6d83a4d0ad3c8b3609839bfbc3ada6d7a2
byoungjoonim <fortress0427@gmail.com>
2020-01-30 15:14:19 +0900

    [refactor] removed attributes which are about ip and time from all tables.

--- src/Django/scode/judge/models.py
+++ src/Django/scode/judge/models.py
@@ -28,43 +28,18 @@ class Subject(models.Model):
 
     lang_seq = models.ForeignKey(Language, on_delete=models.CASCADE)
 
-    # we need to make it remove redundancy
-    input_id = models.CharField(max_length=45)
-    input_ip = models.CharField(max_length=45)
-    input_date = models.DateTimeField(auto_now_add=True, null=True)
-    update_id = models.CharField(max_length=45)
-    update_ip = models.CharField(max_length=45)
-    update_date = models.DateTimeField(auto_now=True, null=True)
     class Meta:
         unique_together = ('year', 'semester', 'subject_cd', 'classes')
-
 
 class Student(models.Model):
     student_id = models.CharField(max_length=10, primary_key=True)
     student_name = models.CharField(max_length=45)
     password = models.CharField(max_length=64)
 
-    # we need to make it remove redundancy
-    input_id = models.CharField(max_length=45)
-    input_ip = models.CharField(max_length=45)
-    input_date = models.DateTimeField(auto_now_add=True)
-    update_id = models.CharField(max_length=45)
-    update_ip = models.CharField(max_length=45)
-    update_date = models.DateTimeField(auto_now=True)
-
-
 class Professor(models.Model):
     professor_id = models.CharField(max_length=10, primary_key=True)
     professor_name = models.CharField(max_length=45)
     password = models.CharField(max_length=64)
-
-    # we need to make it remove redundancy
-    input_id = models.CharField(max_length=45)
-    input_ip = models.CharField(max_length=45)
-    input_date = models.DateTimeField(auto_now_add=True)
-    update_id = models.CharField(max_length=45)
-    update_ip = models.CharField(max_length=45)
-    update_date = models.DateTimeField(auto_now=True)
 
 class Signup_class(models.Model):
     # Table must have one key in Django
@@ -72,14 +47,6 @@ class Signup_class(models.Model):
     # sub_seq same as year, semester, subject_cd, classes
     sub_seq = models.ForeignKey(Subject, on_delete=models.CASCADE)
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
-
-    # we need to make it remove redundancy
-    input_id = models.CharField(max_length=45)
-    input_ip = models.CharField(max_length=45)
-    input_date = models.DateTimeField(auto_now_add=True)
-    update_id = models.CharField(max_length=45)
-    update_ip = models.CharField(max_length=45)
-    update_date = models.DateTimeField(auto_now=True)
 
 class Subject_has_professor(models.Model):
     # Table must have one key in Django
@@ -90,15 +57,6 @@ class Subject_has_professor(models.Model):
     represent_yn = models.BooleanField(default=False)
     # we need to add a field that has size of assignment here.
 
-    # we need to make it remove redundancy
-    input_id = models.CharField(max_length=45)
-    input_ip = models.CharField(max_length=45)
-    input_date = models.DateTimeField(auto_now_add=True)
-    update_id = models.CharField(max_length=45)
-    update_ip = models.CharField(max_length=45)
-    update_date = models.DateTimeField(auto_now=True)
-
-
 class Assignment(models.Model):
     not_use_pri_key = models.AutoField(primary_key=True)
     sequence = models.IntegerField(null=False)
@@ -108,14 +66,6 @@ class Assignment(models.Model):
     assignment_desc = models.TextField()
     deadline = models.DateTimeField(null=False)
     max_score = models.IntegerField()
-	
-    # we need to make it remove redundancy
-    input_id = models.CharField(max_length=45)
-    input_ip = models.CharField(max_length=45)
-    input_date = models.DateTimeField(auto_now_add=True)
-    update_id = models.CharField(max_length=45)
-    update_ip = models.CharField(max_length=45)
-    update_date = models.DateTimeField(auto_now=True)
 
 class Submit(models.Model):
     # Table must have one key in Django
