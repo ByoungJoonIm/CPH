bfba6e3c42db3b01cca980ba6d2f3be29e035bcd
byoungjoonim <fortress0427@gmail.com>
2020-02-01 13:52:47 +0900

    [refactor] professor main page works well. It is written by django style query

--- src/Django/scode/judge/urls.py
+++ src/Django/scode/judge/urls.py
@@ -1,1 +1,9 @@
+from django.conf.urls import url
+from django.contrib import admin
 
+from judge.views import *
+
+urlpatterns = [
+    # example : /professor
+    url(r'^professor$', ProfessorMainLV.as_view(), name='professor'),
+]
