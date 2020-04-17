919a1bb15603e138f8a80eab9e38c87c457e7fff
byoungjoonim <fortress0427@gmail.com>
2020-04-17 14:34:44 +0000

    [temp] Creating auto_conf is commented now

--- src/Django/scode/initializer/essential/auto_init_model.sh
+++ src/Django/scode/initializer/essential/auto_init_model.sh
@@ -8,9 +8,10 @@ mysql < $epath/recreate_db
 python3 manage.py makemigrations
 python3 manage.py migrate
 
-echo "Creating config file..."
-python3 $epath/initialize_auto_conf.py
-echo "Config file was created!"
+# This block will uncomment when project is done.
+#echo "Creating config file..."
+#python3 $epath/initialize_auto_conf.py
+#echo "Config file was created!"
 
 rm -r ~/assignment_cache > /dev/null 2>&1
 python3 $epath/create_directories.py
