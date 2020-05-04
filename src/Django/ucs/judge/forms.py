3bdf438148290c3ecdd34fe10d0191330139754a
byoungjoonim <fortress0427@gmail.com>
2020-05-04 19:15:20 +0900

    [refact] TextArea widget size adjusted in forms.py

--- src/Django/ucs/judge/forms.py
+++ src/Django/ucs/judge/forms.py
@@ -13,7 +13,7 @@ class StudentSubjectAddForm(forms.Form):
     
 class AssignmentForm(forms.Form):
     assignment_name = forms.CharField(label="assignment_name")
-    assignment_desc = forms.CharField(label="assignment_desc", widget=forms.Textarea)
+    assignment_desc = forms.CharField(label="assignment_desc", widget=forms.Textarea(attrs={'rows' : '10','cols' : '100'}))
     assignment_deadline = forms.IntegerField(label="assignment_deadline", initial=7)
     assignment_in_file = forms.FileField(label="assignment_in_file")
     assignment_out_file = forms.FileField(label="assignment_out_file")
